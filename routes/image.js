b28bd5ef44c1a6a805777b2592f67365525d6c33
Ed Irvine <edwin.irvine@edu.sait.ca>
2021-03-24 14:57:52 -0600

    got single image display to work properly

--- routes/image.js
+++ routes/image.js
@@ -4,40 +4,36 @@ const Guitar = require('../models/guitar.js')
 
 const router = express.Router()
 
-//  Problem with this block of code
+// Sorts through the Guitar database for an id item that matches the id parameter in the URL
 router.get('/images/:id', function (req, res) {
-  const guitar = Guitar.find(function (item) {
-//  return item.id = req.params.id            //  crashes server TypeError: Cannot read property 'id' of null
-    return item === req.params.id             //  See Notes 
-  })
+  Guitar.findOne({id: req.params.id}, function (err, item) {
   
   //  Error handling if .find comes back false (found no matching value)
-  if (!guitar) {
-    res.status(404)
-    res.sendFile(__dirname +'/public/404.html')
-  }
+    if (err || item == 0) {
+     res.status(404)
+      res.sendFile(__dirname +'/public/404.html')
+    }
 
   //  Renders the page "single-item.ejs" and passes the variables
-  res.render('pages/single-item.ejs', {
-    pageTitle: 'CPNT262-A5 Single Image Page',
-    guitarTitle: guitar.title,
-    guitarSRC: guitar.pathURL,
-    guitarAlt: guitar.description
+    res.render('pages/single-item.ejs', {
+      pageTitle: 'CPNT262-A5 Single Image Page',
+      guitar: item
+    })
   })
-
- })
+})
 
 module.exports = router
 
+
 //  Attribution
 //  Sample Code
-//  https://github.com/sait-wbdv/sample-code/blob/master/backend/express/route-params/_finished/routes/index.js
+//  https://github.com/sait-wbdv/in-class/blob/main/w10r/2-definitions-atlas/routes/api-v0.js
 
 //  **********************************************  NOTES  **************************************************************
 //
 //  Logic Error:
 //  Problem with item.id in .find() loop
 //  item is returning null, and then trying to find a .id item of a null value
-//  This leads me to believe that the .find is not reading my database data correctly
+//  This leads me to believe that the .find is not reading my database correctly
 //
 //  *********************************************************************************************************************
